78f399fc3f0d9c9bc8a4363e4256f2566a4f8e1b
luizfelip07 <131314701+luizfelip07@users.noreply.github.com>
2023-11-13 11:37:44 -0300

    aaa

---
+++ script.js
@@ -0,0 +1,10 @@
+let expression = '';
+
+function appendNumber(Number);
+  expression += Number;
+  document.getElementById('result').value = expression;
+}
+
+function appendNumber(operator) {
+    expression += operator;
+    document.getElementById('result').vale = expression;
