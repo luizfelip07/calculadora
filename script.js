d24d36f0596f9e042c60536fe40d084b775a96fe
luizfelip07 <131314701+luizfelip07@users.noreply.github.com>
2023-11-27 11:40:54 -0300

    Luiz Felipe

--- script.js
+++ script.js
@@ -7,4 +7,21 @@ function appendNumber(Number);
 
 function appendNumber(operator) {
     expression += operator;
-    document.getElementById('result').vale = expression;
+    document.getElementById('result').vale= expression;
+  }
+
+function calculate(){
+   try{
+       const result = eval(expression);
+       document.getElementById('result').value = result;
+       expression = '';
+ } catch (error) {
+ document.getElementById('result').value = 'Error';
+ expression = '';
+ }
+}
+       function clear(): void
+function clear() {
+  expression = '';
+  document.getElementById('result').value = '';
+}
